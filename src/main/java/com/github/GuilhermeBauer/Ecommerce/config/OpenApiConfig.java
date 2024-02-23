70a7e62aae750b4faf248588a89ffce71af43f50
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2024-02-23 20:00:34 -0300

    feat: adding swagger project

---
+++ src/main/java/com/github/GuilhermeBauer/Ecommerce/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.github.GuilhermeBauer.Ecommerce.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-commerce API")
+                        .version("v1")
+                        .description("An E-commerce API")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")
+                                        .url("https://springdoc.org/")
+                        ));
+    }
+}
